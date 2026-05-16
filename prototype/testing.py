b4bde338ea1d29f80b9e4a964554308a1acc297c
nickytanyongjun <ntyj6962@gmail.com>
2026-05-16 11:53:25 +0800

    testing.py

--- prototype/testing.py
+++ prototype/testing.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCkH2hiqSJNes31u7GOqbatmdaY9KbXAjE")
+genai.configure(api_key="My APi Key")
 
 for model in genai.list_models():
     print(model.name)
